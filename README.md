6de3fe3a5a3be0750707135d316779d62899e518
Lorena Nogueira <lorinsantanna@gmail.com>
2025-02-27 20:04:48 -0300

    corrige erros apontados na revisão

--- README.md
+++ README.md
@@ -5,16 +5,20 @@ O **Around The U.S.** é uma página interativa onde os usuários podem adiciona
 ## 🚀 Funcionalidades
 
 - **Perfil do usuário:** Adicione informações pessoais como nome e uma breve descrição sobre você.
-- **Edição de perfil:** Modifique seu nome e a descrição sobre você diretamente na página.
-- **Galeria de fotos:** Veja imagens de lugares icônicos como o Vale de Yosemite, Lago Louise, Montanhas Carencro, entre outros.
+- **Galeria de fotos:** Veja fotos de paisagens de lugares como o Vale de Yosemite, Lago Louise, Montanhas Carencro, entre outros.
 - **Interatividade:** Curta fotos da galeria.
 
 ## 🛠️ Tecnologias Usadas
 
-- **HTML:** Estrutura básica da página.
-- **CSS:** Estilização da página e elementos interativos.
-- **JavaScript:** Funcionalidade de interação com o usuário (editar perfil, curtir/remover fotos).
+- **HTML:** Estrutura do site.
+- **CSS:** Estilização e design da página.
+- **JavaScript:** Funcionalidade de interação com o usuário (abrir/fechar popup e editar perfil).
 - **Responsividade:** O design foi feito para ser responsivo, adaptando-se a diferentes tamanhos de tela, com no mínimo 320px de largura.
 
 ## 🌐 Acesse o site:
+
 https://lorinogueira.github.io/web_project_around/
+
+## 👩‍💻 Autor
+
+Desenvolvido por Lorena Nogueira.
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
-let container = document.querySelector(".page");
-let popup = container.querySelector(".popup");
+const container = document.querySelector(".page");
+const popup = container.querySelector(".popup");
 popup.classList.remove("popup_opened");
 
-let formElement = container.querySelector(".popup__form");
-let nameInput = container.querySelector(".popup__input_content_name");
-let aboutmeInput = container.querySelector(".popup__input_content_aboutme");
-let nameProfile = container.querySelector(".profile__name");
-let aboutmeProfile = container.querySelector(".profile__aboutme");
+const formElement = container.querySelector(".popup__form");
+const nameInput = container.querySelector(".popup__input_content_name");
+const aboutmeInput = container.querySelector(".popup__input_content_aboutme");
+const nameProfile = container.querySelector(".profile__name");
+const aboutmeProfile = container.querySelector(".profile__aboutme");
 
-let editButton = container.querySelector(".profile__edit-button");
-let closeButton = container.querySelector(".popup__close-button");
-let submitButton = container.querySelector(".popup__submit-button");
+const editButton = container.querySelector(".profile__edit-button");
+const closeButton = container.querySelector(".popup__close-button");
+const submitButton = container.querySelector(".popup__submit-button");
 
 function openPopup() {
   popup.classList.add("popup_opened");
